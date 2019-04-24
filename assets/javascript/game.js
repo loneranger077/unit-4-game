7baf1532a5709db23adcf3f47c0de872ffe768f2
nguzak3 <nick.guzak@gmail.com>
2019-04-23 21:51:34 -0400

    hello again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,76 @@
 var playerScore = 0;
 var compScore = 0;
 var tempScore = 0;
+    
+var maxScore = Math.floor((Math.random()*101)+19);
+
+var fst = Math.floor((Math.random()*11)+1);
+var sec = Math.floor((Math.random()*11)+1);
+var thi = Math.floor((Math.random()*11)+1);
+var fou = Math.floor((Math.random()*11)+1);
 
 $(document).ready(function() {
 
-var maxScore = Math.floor((Math.random()*101)+19);
+var checker = scoreChecker;
 
-$("div.thing").text(maxScore);
-
-var fst = Math.floor(Math.random*10);
-var sec = Math.floor(Math.random*10);
-var thi = Math.floor(Math.random*10);
-var fou = Math.floor(Math.random*10);
-
-while (tempScore<maxScore); {
+$("div.thing").text("The score to match is : " + maxScore);
 
 $(first).on("click", function first () {
-    tempScore = tempScore + fst;
-    $("div.thing").text(tempScore);
+    tempScore += fst;
+    $("div.thing2").text("Your current score is: " + tempScore);
+    checker();
 });
 
 $(second).on("click", function second () {
-    tempScore = tempScore + sec;
-    $("div.thing").text(tempScore);
+    tempScore += sec;
+    $("div.thing2").text("Your current score is: " + tempScore);
+    checker();
 });
 
 $(third).on("click", function third () {
-    tempScore = tempScore + thi;
-    $("div.thing").text(tempScore);
+    tempScore += thi;
+    $("div.thing2").text("Your current score is: " + tempScore);
+    checker();
 });
 
 $(fourth).on("click", function fourth () {
-    tempScore = tempScore + fou;
-    $("div.thing").text(tempScore);
+    tempScore += fou;
+    $("div.thing2").text("Your current score is: " + tempScore);
+    checker();
 });
 
-}
+function scoreChecker() {
 
-if (tempScore > maxScore); {
-    compScore = compScore + 1;
-    tempScore = 0;
-    $("#losses").text("Losses: " + compScore);
-}
+    if (tempScore > maxScore) {
+        
+        compScore += 1;
+        tempScore = 0;
+        $(".losses").text("Losses: " + compScore);
+        maxScore = Math.floor((Math.random()*101)+19);
+        fst = Math.floor((Math.random()*11)+1);
+        sec = Math.floor((Math.random()*11)+1);
+        thi = Math.floor((Math.random()*11)+1);
+        fou = Math.floor((Math.random()*11)+1);
+        $("div.thing").text("The score to match is : " + maxScore);
+        $("div.thing2").text(tempScore);
 
-if (tempScore = maxScore); {
-    playerScore = playerScore + 1;
-    tempScore = 0;
-    $("#wins").text("Wins: " + playerScore)
-}
+    };
+
+    if (tempScore === maxScore) {
+        
+        playerScore += 1;
+        tempScore = 0;
+        $(".wins").text("Wins: " + playerScore);
+        maxScore = Math.floor((Math.random()*101)+19);
+        fst = Math.floor((Math.random()*11)+1);
+        sec = Math.floor((Math.random()*11)+1);
+        thi = Math.floor((Math.random()*11)+1);
+        fou = Math.floor((Math.random()*11)+1);
+        $("div.thing").text("The score to match is : " + maxScore);
+        $("div.thing2").text(tempScore);
+
+    };
+
+};
 
 });
